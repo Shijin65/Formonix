9bb841ee62eb1e145df100d7a55217a50ed7eb6f
hafishali <hafishali4@gmail.com>
2024-08-06 09:13:49 +0530

    'use_of_statis_files'

--- formonix backend/index.js
+++ formonix backend/index.js
@@ -6,6 +6,8 @@ const cors=require('cors')
 
 const router=require('./routers/router')
 
+const path = require('path');
+
 // body parser
 const bodyparse=require('body-parser')
 
@@ -34,6 +36,12 @@ profoserver.use(bodyparse.json())
 // access the document folder
 
 profoserver.use('/documents',express.static('./documents'))
+
+profoserver.use(express.static(path.join(__dirname, '../cliend/build')));
+
+profoserver.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../cliend/build', 'index.html'));
+});
 
 const PORT= process.env.PORT || 4010 
 
